ad37184e305225aa0ec90363b57e2ac41fdcfbc2
shireesha <shireesha@quintype.com>
2017-11-26 16:53:45 +0530

    fix another typo

--- src/pages/community.js
+++ src/pages/community.js
@@ -7,7 +7,7 @@ const Community = () => (
       I like to promote community learning and contributed to <a href="https://djangogirls.org/" target="_blank">Django Girls</a> as a coach in two location
       Bangalore & Ahemdabad
     </p>
-    <p> Django Girls is a program run world wide to promote girls to program.</p>
+    <p> Django Girls is a program run world wide to promote girls to code.</p>
   </div>
 )
 
