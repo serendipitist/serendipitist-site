cf557acd2757ac956599423b1e0916d0296e3630
shireesha <shireesha@quintype.com>
2017-11-27 06:51:34 +0530

    fix the text

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,8 +3,7 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-   <h3> I like working on Front end technologies. Currently working for digital publishing platform Startup in Bangalore.</h3>
-    <h3>When I get some time I like building stuff and improve my tech skills </h3>
+   <p> I like working on Front end technologies. Currently working for digital publishing platform Startup in Bangalore.</p>
   </div>
 )
 
