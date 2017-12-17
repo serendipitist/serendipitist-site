59c70931492f203b360552bc88ea2604fdb8e345
shireesha <shireesha@quintype.com>
2017-12-17 09:40:31 +0530

    update font google analytics and about me

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Serendiptist`,
+    discription: `blog`
   },
   plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: "UA-106888717-1",
+      // Setting this parameter is optional
+      anonymize: true,
+    },
+  },
+],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,9 @@ const greetings = () => {
 const IndexPage = () => (
   <div>
     <h1>Hello people,</h1>
-    <p>My name is Shireesha and I am from India. I 💓 Web. If I am not infront of screen ,I love exploring natural landscape.</p>
+    <p>My name is Shireesha . I am a front end developer . I 💓 Web and like to build web apps. I have worked on tradition MVC oriented frameworks  like Laravel and Django. However Most of my stack is based around the ever growing Node.js and Javascript ecosystem. Most of day to day work involves building and managing user interface of web using  HTML , CSS and Javascript  technologies. I love trying and learning new frameworks.
+      I am  from  Bangalore. and love my city.
+      While , I am not infront of my screen. I like to go on hiking/trekking or take a long walk.</p>
     <p>Click Here to know about my <Link to="/work/">Work</Link>👩‍💻</p>
     <p> I like to promote <Link to="/community/">Community</Link> Learning</p>
     <p>You can 📨 <Link to="/contact/">contact</Link></p>
