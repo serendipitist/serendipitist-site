704635f01a692c597db9cd65e180fa8f6dd20d36
Shireesha PS <serendipitist@users.noreply.github.com>
2018-06-20 21:17:10 +0530

    Update work.js

--- src/pages/work.js
+++ src/pages/work.js
@@ -6,7 +6,7 @@ const SecondPage = () => (
   <article>
      <p> I like working on Front end technologies.</p>
      <p> Currently working for digital publishing platform Startup in Bangalore.</p>
-     <p> Listing my side project <a href="https://serendipitist.github.io/cricscore/">Demo Web component </a> </p>
+     <p> Listing my side project <a href="https://serendipitist.github.io/bulletin-board/">Demo Web component </a> </p>
    </article>
   </div>
 )
