d04b3d684596c9cd9393ebe3f763d72f05f1dc19
serendipitist <ps.shireesha@gmail.com>
2018-07-03 17:36:17 +0530

    update pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ const greetings = () => {
 const IndexPage = () => (
   <div>
     <h1>Hello people,</h1>
-    <p>My name is Shireesha . I am a front end developer . I 💓 Web and like to build web apps. I have worked on tradition MVC oriented frameworks  like Laravel and Django. However Most of my stack is based around the ever growing Node.js and Javascript ecosystem. Most of day to day work involves building and managing user interface of web using  HTML , CSS and Javascript  technologies. I love trying and learning new frameworks.
-      I am  from  Bangalore. and love my city.
+    <p>My name is Shireesha. I 💓 Web and like to build web apps. I have worked on traditional MVC oriented frameworks  like Laravel and Django. However Most of my stack is based around the ever growing Node.js and Javascript ecosystem. Most of day to day work involves building and managing user interface of web using  HTML , CSS and Javascript  technologies. I love trying and learning new frameworks.
+      I am from  Bangalore and love my city.
       While , I am not infront of my screen. I like to go on hiking/trekking or take a long walk.</p>
     <p>Click Here to know about my <Link to="/work/">Work</Link>👩‍💻</p>
     <p> I like to promote <Link to="/community/">Community</Link> Learning</p>
--- src/pages/work.js
+++ src/pages/work.js
@@ -3,11 +3,11 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-  <article>
-     <p> I like working on Front end technologies.</p>
-     <p> Currently working for digital publishing platform Startup in Bangalore.</p>
-     <p> Listing my side project <a href="https://serendipitist.github.io/bulletin-board/">Demo Web component </a> </p>
-   </article>
+    <article>
+      <p> I like working on Front end technologies.</p>
+      <p>Actively looking out for opportunity in Front End Technologies. Available for hire</p>
+      <p> Listing my side project <a href="https://serendipitist.github.io/bulletin-board/">Demo Project </a> </p>
+    </article>
   </div>
 )
 
